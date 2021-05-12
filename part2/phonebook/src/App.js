102c3bb670191b8bf7abb98a8fb9ffe615df03ac
Pavel Pashov <pashov.pavel@gmail.com>
2021-05-12 21:38:42 +0300

    Solution for 2.6: The Phonebook Step1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const Name = ({ name }) => {
+  return (
+    <div>{name}</div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const person = { name: newName }
+    setPersons(persons.concat(person))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>debug: {newName}</div>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>
+        <Name key={person.name} name={person.name} />
+      )}
+    </div>
+  )
+}
+
+export default App
