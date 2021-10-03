56f2310f805c0408737997e5b14696de4d7e7e23
Pavel Pashov <pashov.pavel@gmail.com>
2021-10-03 18:10:39 +0300

    4.3: helper functions and unit tests, step1

--- part4/index.js
+++ part4/index.js
@@ -1,8 +1,5 @@
 const app = require('./app') // varsinainen Express-sovellus
-const http = require('http')
 const config = require('./utils/config')
-
-const server = http.createServer(app)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
---
+++ part4/utils/list_helper.js
@@ -0,0 +1,10 @@
+const dummy = (blogs) => {
+  blogs.forEach(blog => {
+    console.log(blog)
+  })
+  return 1
+}
+
+module.exports = {
+  dummy
+}
