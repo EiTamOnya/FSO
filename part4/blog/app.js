a0ccb78a29d5fc351a47e014cf9ec09a57b55675
EiTamOnya <pashov.pavel@gmail.com>
2021-10-07 17:49:05 +0300

    4.15: bloglist expansion, step3

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,16 +1,16 @@
 
 const config = require('./utils/config')
-const blogRouter = require('./controllers/blog')
+const blogRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 const express = require('express')
 const app = express()
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const cors = require('cors')
-
 const mongoose = require('mongoose')
 
 const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     logger.info('connected to MongoDB')
   })
@@ -23,6 +23,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
