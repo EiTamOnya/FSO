20caffa1120afa816ec2d16f86c7f58992c9b35e
EiTamOnya <pashov.pavel@gmail.com>
2021-10-08 12:24:17 +0300

    4.16*: bloglist expansion, step4

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -2,21 +2,31 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
+usersRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  if (body.password.length <= 3) {
+    response.status(400).send({ error: 'password too short' })
+  }
+  else {
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  })
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-  const savedUser = await user.save()
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    })
 
-  response.json(savedUser)
+    try {
+      const savedUser = await user.save()
+      response.json(savedUser)
+    } catch (exception) {
+      next(exception)
+    }
+
+  }
 })
 
 usersRouter.get('/', async (request, response) => {
