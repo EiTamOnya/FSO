cdd7ac39026f2c64d52bc0dd723461255001f1c5
EiTamOnya <pashov.pavel@gmail.com>
2021-10-09 21:18:39 +0300

    5.14*: Blog list tests, step2

--- part5/bloglist-frontend-main/src/components/Blog.test.js
+++ part5/bloglist-frontend-main/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -18,9 +18,29 @@ test('renders content', () => {
   const likes = component.container.querySelector('.likes')
   const url = component.container.querySelector('.url')
 
-  component.debug()
   expect(title).toHaveTextContent(blog.title)
   expect(author).toHaveTextContent(blog.author)
   expect(likes).toHaveTextContent('likes:')
   expect(url).not.toHaveTextContent()
+})
+
+test('click button to see likes and url', () => {
+  const blog = {
+    title: 'Test title',
+    author: 'Test author',
+    likes: 13,
+    url: 'test.com'
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+
+  const secondDiv = component.container.querySelector('.title ~ div')
+  const button = component.container.querySelector('.title button')
+
+  expect(secondDiv).toHaveStyle('display: none')
+  fireEvent.click(button)
+  expect(secondDiv).toHaveStyle('display: block')
 })
