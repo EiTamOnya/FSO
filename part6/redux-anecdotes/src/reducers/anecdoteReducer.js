c0cbb34f981d0c2f5de8b8cf6bd9860c89acbd0a
EiTamOnya <pashov.pavel@gmail.com>
2021-10-14 22:14:01 +0300

    6.13 Anecdotes and the backend, step1

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,35 +1,23 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
     case 'VOTE':
       const id = action.data.id
       let updatedAnecdote = state.find(obj => obj.id === id)
       updatedAnecdote.votes += 1
       return state.map(anecdote => anecdote.id === id ? updatedAnecdote : anecdote)
+    case 'INIT_ANECDOTES':
+      return action.data
     case 'CREATE':
       return state.concat(action.data)
     default:
       return state
+  }
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes,
   }
 }
 
@@ -45,11 +33,7 @@ export const voteAnecdote = (id) => {
 export const createAnecdote = (anecdote) => {
   return {
     type: 'CREATE',
-    data: {
-      content: anecdote,
-      id: getId(),
-      votes: 0
-    }
+    anecdote,
   }
 }
 
