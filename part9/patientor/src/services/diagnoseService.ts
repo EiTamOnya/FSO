3645eacd575f023044c2a5b1c60be4d5a243a685
EiTamOnya <pashov.pavel@gmail.com>
2021-10-28 16:28:33 +0300

    9.10: Patientor backend, step3

---
+++ part9/patientor/src/services/diagnoseService.ts
@@ -0,0 +1,12 @@
+import diagnoseData from '../../data/diagnoses';
+import { Diagnose } from '../types';
+
+const diagnoses: Diagnose[] = diagnoseData;
+
+const getEntries = (): Diagnose[] => {
+  return diagnoses;
+};
+
+export default {
+  getEntries,
+};
