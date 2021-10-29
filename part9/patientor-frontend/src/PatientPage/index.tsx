0358f613fb0c3be487995c8bb8ad339352b32216
EiTamOnya <pashov.pavel@gmail.com>
2021-10-29 22:35:35 +0300

    9.17: patientor, step2

---
+++ part9/patientor-frontend/src/PatientPage/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {Icon} from 'semantic-ui-react';
+import { useParams } from "react-router-dom";
+import { apiBaseUrl } from "../constants";
+import { Patient } from "../types";
+import { useStateValue } from "../state";
+
+const PatientPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [{patients}, dispatch] = useStateValue();
+  const [patient, setPatient] = useState<Patient | undefined>(Object.values(patients).find(p => p.id === id));
+  
+  
+  useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: patient } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: "SET_PATIENT", payload: patient });
+        setPatient(patient);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    if (patient === undefined || !('ssn' in patient)){
+      console.log(patient);
+      void fetchPatient();
+    }
+  }, [dispatch]);
+
+  const iconName = patient?.gender === 'male' ? 'mars' : 'venus'; 
+  
+  return (
+    <div className="App">
+      <h1>{patient?.name}<Icon name={iconName}/></h1> 
+      <p>ssn: {patient?.ssn}</p>
+      <p>occupation: {patient?.occupation}</p>
+    </div>
+  );
+};
+
+export default PatientPage;
